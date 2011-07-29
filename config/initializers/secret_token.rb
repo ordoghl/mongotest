1a89028b26858da6041d595f2ef0e5bb56d87138
Laszlo Ordogh <ordoghl@gmail.com>
2011-07-29 14:06:14 +0200

    initial rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mongotest::Application.config.secret_token = 'b3b98f85b0db30141d8ddc3cec28691b253c1eb339e27a3921506a903d591511d2bd6d54fe07cbdd062931544058c49265834aad412786e76c5ae38017bdd58d'
